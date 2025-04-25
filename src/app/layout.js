15b02221827a6849858591afcb49d1bc618cb398
MoazEb <moazm942@gmail.com>
2025-04-25 15:03:34 +0300

    add analytics & speedInsigths

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 // import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Footer from "@/components/Footer";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -16,6 +18,8 @@ export default function RootLayout({ children }) {
       >
         <Header />
         {children}
+        <Analytics />
+        <SpeedInsights />
         <Footer />
 
       </body>
